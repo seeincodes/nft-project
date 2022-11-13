518a091623d91c2000ac54c4b699834b2212fed9
“seeinplays” <seeinplays@gmail.com>
2022-11-13 00:53:39 -0700

    basic NFT contract and switch to goerli testnet

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -4,8 +4,42 @@ pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract MyEpicNFT is ERC721 {
+    // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("NFT contract");
+    }
+
+    // A function our user will hit to get their NFT.
+    function makeAnEpicNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+    }
+
+    // Set the NFT's metadata
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(_exists(_tokenId));
+        return "https://jsonkeeper.com/b/6FD8";
     }
 }
